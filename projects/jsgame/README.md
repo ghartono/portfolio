4bed170426d7cdc094da1068ba75a5cefc4275ff
Gionaldo Hartono <Gionaldo@Gionaldos-MacBook-Pro.local>
2018-07-13 04:38:30 -0700

    fix4

--- projects/jsgame/README.md
+++
@@ -1,3 +0,0 @@
-# Js Game
-
-This was the final assignment for a JavaScript class I took in 2015
--- projects/jsgame/canvas2.js
+++
@@ -1,9 +0,0 @@
-
-function timeLoop()
-{
-	var a = "";
-	for( var i = 0; i < 10; i++)
-	{
-		document.getElementById("a").innerHTML = Math.floor( Math.random() * 9) + 1);
-	}
-}
