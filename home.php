b197baa2f8647117567fcb30471abcece7209031
Gionaldo Hartono <Gionaldo@Gionaldos-MacBook-Pro.local>
2018-07-13 03:48:37 -0700

    resume website

--- home.php
+++ home.php
@@ -1,48 +1,146 @@
-<?php include "header.html"; ?>
-    <div class="container top-buffer">
-        <h2 class="center">Who</h2>
-        <div class="row top-buffer">
-          <div class="col-xs-12 col-sm-12 col-md-4 center">
-            <i class="fas fa-briefcase"></i><p>I am Gionaldo Hartono, a Software Engineer.<br> I mainly develop websites along with the UI/UX side, but am open to work on other stuff.</p>
-          </div>
-          <div class="col-xs-12 col-sm-12 col-md-4 center">
-            <i class="fas fa-user-graduate"></i><p>BSc, Computing Science.<br>Simon Fraser University</p>
-          </div>
-          <div class="col-xs-12 col-sm-12 col-md-4 center" id="myinfo">
-            <i class='far fa-question-circle'></i><p>Born in Indonesia.<br>I got interested in programming during high school and decided to pursue a Computer Science degree in Canada.<br>Other interests include gaming, DJing, and drawing (and of course programming!)</p>
-          </div>
-        </div>
-        <div class="even">
-        <h2 class="center top-buffer">Where</h2>
-        <div class="row top-buffer">
-            <div class="col-xs-12 col-sm-12 col-md-12 center">
-            <p><i class="fas fa-map-marker-alt"></i> Vancouver, BC. Canada</p>
-            </div>
-        </div>
-        </div>
-        <h2 class="center top-buffer">What</h2>
-        <div class="row top-buffer">
-            <div class="col-xs-12 col-sm-12 offset-md-2 col-md-8 center">
-            <p>This website is created to showcase my <b><a href="/project">portfolio</a></b> and <b><a href="/resume">resume</a></b>, but I will do some web-app developments in the <b><a href="/experiment"> Experiment</a></b> section! <b><a href="/project">Project</a></b> section is where I will store all my work, whether it's on hiatus or finished.</p>
-            </div>
+<html><head>
+    <title>Gionaldo Hartono's Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro|Spectral+SC|Spectral" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/stylesheet/css/fonts.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script><style></style>
+</head>
+<body>
+
+
+<header id="top">
+    <div class="container font-header row">
+        <div class="logo col-md-6 col-xs-12 col-sm-12">
+            <a href="/" class="nohover">Gionaldo Hartono</a>
         </div>
 
-        <div class="even">
-        <div class="row top-buffer">
-            <div class="col-xs-12 col-sm-12 col-md-4 center">
-                <h3 class="center top-buffer">When</h3>
-                <p> Started on June 3, 2018, <br> Website has been up since June 6</p>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-4 center">
-                <h3 class="center top-buffer">Why</h3>
-                <p> I am currently looking for a job! "A website is nice to have," I thought. <br>If you do have a job offer, you can see my contact info <b><a href="/contact">here</a></b></p>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-4 center">
-                <h3 class="center top-buffer">How</h3>
-                <p> This website is powered by Bootstrap, simple HTML/CSS and hosted using Google Cloud Platform<br>
-                <p> You can find a work-in-progress NodeJS website which you can find in <a href="http://github.com/ghartono/mywebsite">here</a></p>
-            </div>
-        </div>
+        <!-- Desktop Global Navigation -->
+        <div class="col-md-6">
+            <nav class="col-sm-hidden col-md-visible right inline">
+<p>2210 Dauphin Pl, Burnaby, BC, Canada, V5B4G9<br>
++1 (604) 710-5886<br>
+    gionaldoh@yahoo.com
+</p>
+            </nav>
         </div>
     </div>
+</header>
+
+<hr>
+<hr>
+
+<section id="content">
+    <div class="loading">
+        <span class="loader"></span>
+    </div>
+</section>
+
+
+
+    <div class="container">
+      <div class="row top-buffer">
+        <div class="col-xs-12 col-sm-12 col-md-12 center">
+            <h2>Skills</h2>
+        </div>
+        <div class="col-xs-12 col-sm-12 offset-md-3 col-md-6">
+            <p><b>Language &amp; runtime</b><br>
+            HTML/CSS, PHP, JavaScript, Java, C++, Ruby, NodeJS</p>
+            <p><b>Frameworks</b><br>
+            Laravel, Rails, Bootstrap, CodeIgniter</p>
+            <p><b>Database</b><br>
+            MySQL, PostgreSQL</p>
+            <p><b>OS</b><br>
+            Windows, Linux, MacOS</p>
+            <p><b>Others</b><br>
+            Github, Heroku, VMWare, VirtualBox</p>
+        </div>
+      </div>
+
+      <div class="even">
+      <div class="row top-buffer">
+        <div class="col-xs-12 col-sm-12 col-md-12 center">
+            <h2>Experience</h2>
+        </div>
+        <div class="col-xs-12 col-sm-12 offset-md-3 col-md-6">
+            <h3>Work</h3>
+<p>
+            <b>Web Developer,</b> Finger Technology<br>
+            <i class="far fa-question-circle"></i>Internship where I created features for their website and learned CodeIgniter's basic how-to<br>
+            <i class="far fa-comment"></i>JavaScript, PHP, CodeIgniter, MySQL<br>
+            
+            </p>
+
+            <p>
+    </p><h3>Projects</h3><p>
+            <b>Online Portfolio</b>, gio-portfolio.appspot.com<br>
+            <i class="far fa-question-circle"></i>Portfolio website that I made recently, still in progress.<br>
+            <i class="far fa-comment"></i>HTML/CSS, PHP (live), NodeJS (WIP)<br>
+            
+                        
+            </p>
+<p><p>
+            <b>MasterChef</b>, masterchef-group-3.herokuapp.com<br>
+            <i class="far fa-question-circle"></i>Group project for CMPT276, Introduction to Software Engineering, in an agile development environment.<br>
+            <i class="far fa-comment"></i>Ruby on Rails, PostgreSQL, Heroku<br>
+            
+                        
+            </p></p>
+<p>
+    <b>JavaScript Web-Game</b><br>
+    <i class="far fa-question-circle"></i>Final assignment in a JavaScript class I took in 2015. Had fun creating all the sprite characters!
+<br><i class="far fa-comment"></i>JavaScript, HTML5 Canvas
+    
+</p><p>
+            <b>PHP/MySQL Web-based payroll system</b><br>
+            <i class="far fa-question-circle"></i>My first ever programming project! Although badly designed, it was a good experience of the whole system-development-life-cycle process. Documentations are available upon request.<br>
+            <i class="far fa-comment"></i>HTML/CSS, PHP, JavaScript, MySQL<br>
+            
+            </p>
+
+
+            
+        <p>
+            Code available on <a href="https://github.com/ghartono" target="_BLANK"><i class="fab fa-github"></i>github.com/ghartono</a>
+            </p></div>
+      </div>
+
+      </div>
+      <div class="row top-buffer">
+        <div class="col-xs-12 col-sm-12 col-md-12 center">
+            <h2>Education</h2>
+        </div>
+        <div class="col-xs-12 col-sm-12 offset-md-3 col-md-6 center">
+            <p>BSc. Computing science, Simon Fraser University
+            </p>
+        </div>
+      </div>      
+    </div>
+
+<footer>
+    <div class="container font-header row">
+        <!--
+        <div class="col-md-6">    
+        <nav class="col-xs-hidden col-md-visible">
+                <ul>
+                    <li><a class="nohover" href="/resume">Resume</a></li>
+                    <li><a class="nohover" href="/project">Projects</a></li>
+                    <li><a class="nohover" href="/blog">Blog</a></li>
+                    <li><a class="nohover" href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        -->
+        <div class="col-xs-12 col-sm-12 offset-md-4 col-md-4 center top-buffer">
+                
+        </div>
+        
+    </div>
+</footer>
+
+
+
+</body></html>
 <?php include "footer.html"; ?>
